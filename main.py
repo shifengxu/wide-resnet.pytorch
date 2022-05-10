b67d7cbe9e3f01faf270f208f43e6777e40f514c
CSLS34\shifeng <shifeng001@e.ntu.edu.sg>
2022-05-10 14:20:23 +0800

    Adjust main.py, to match my style.

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 from __future__ import print_function
 
-import torch
-import torch.nn as nn
+import os
+import time
+import argparse
+import torch.utils.data
 import torch.optim as optim
-import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
-import config as cf
 
 import torchvision
 import torchvision.transforms as transforms
 
-import os
-import sys
-import time
-import argparse
-import datetime
-
+import config as cf
 from networks import *
-from torch.autograd import Variable
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR-10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning_rate')
+parser.add_argument('--gpu_ids', nargs='+', type=int, default=[1, 2])
 parser.add_argument('--net_type', default='wide-resnet', type=str, help='model')
 parser.add_argument('--depth', default=28, type=int, help='depth of model')
 parser.add_argument('--widen_factor', default=10, type=int, help='width of model')
@@ -32,83 +27,96 @@ args = parser.parse_args()
 
 # Hyper Parameter settings
 use_cuda = torch.cuda.is_available()
+use_cuda_parallel = use_cuda and len(args.gpu_ids) > 1
+device = f"cuda:{args.gpu_ids[0]}" if use_cuda and args.gpu_ids else "cpu"
+
 best_acc = 0
-start_epoch, num_epochs, batch_size, optim_type = cf.start_epoch, cf.num_epochs, cf.batch_size, cf.optim_type
+start_epoch, num_epochs = cf.start_epoch, cf.num_epochs
 
-# Data Uplaod
-print('\n[Phase 1] : Data Preparation')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset]),
-]) # meanstd transformation
+# Data Preparation
+def get_data_loader():
+    t_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset]),
+    ]) # mean-std transformation
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset]),
-])
+    t_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset]),
+    ])
 
-if(args.dataset == 'cifar10'):
-    print("| Preparing CIFAR-10 dataset...")
-    sys.stdout.write("| ")
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_test)
-    num_classes = 10
-elif(args.dataset == 'cifar100'):
-    print("| Preparing CIFAR-100 dataset...")
-    sys.stdout.write("| ")
-    trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
-    testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=False, transform=transform_test)
-    num_classes = 100
+    if args.dataset == 'cifar10':
+        print("| Preparing CIFAR-10 dataset...")
+        sys.stdout.write("| ")
+        train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=t_train)
+        test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=t_test)
+        num_classes = 10
+    elif args.dataset == 'cifar100':
+        print("| Preparing CIFAR-100 dataset...")
+        sys.stdout.write("| ")
+        train_set = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=t_train)
+        test_set = torchvision.datasets.CIFAR100(root='./data', train=False, download=False, transform=t_test)
+        num_classes = 100
+    else:
+        raise Exception(f"Unknown args.dataset: {args.dataset}")
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+    trainloader = torch.utils.data.DataLoader(train_set, batch_size=cf.batch_size, shuffle=True, num_workers=2)
+    testloader = torch.utils.data.DataLoader(test_set, batch_size=100, shuffle=False, num_workers=2)
+    return trainloader, testloader, num_classes
 
-# Return network & file name
-def getNetwork(args):
-    if (args.net_type == 'lenet'):
-        net = LeNet(num_classes)
+def get_model(num_classes):
+    if args.net_type == "lenet":
+        model = LeNet(num_classes)
+    elif args.net_type == 'vggnet':
+        model = VGG(args.depth, num_classes)
+    elif args.net_type == 'resnet':
+        model = ResNet(args.depth, num_classes)
+    elif args.net_type == 'wide-resnet':
+        model = Wide_ResNet(args.depth, args.widen_factor, args.dropout, num_classes)
+    else:
+        print('Error : Network should be either [LeNet / VGGNet / ResNet / Wide_ResNet')
+        sys.exit(0)
+    return model
+
+def get_model_file_name():
+    if args.net_type == 'lenet':
         file_name = 'lenet'
-    elif (args.net_type == 'vggnet'):
-        net = VGG(args.depth, num_classes)
+    elif args.net_type == 'vggnet':
         file_name = 'vgg-'+str(args.depth)
-    elif (args.net_type == 'resnet'):
-        net = ResNet(args.depth, num_classes)
+    elif args.net_type == 'resnet':
         file_name = 'resnet-'+str(args.depth)
-    elif (args.net_type == 'wide-resnet'):
-        net = Wide_ResNet(args.depth, args.widen_factor, args.dropout, num_classes)
+    elif args.net_type == 'wide-resnet':
         file_name = 'wide-resnet-'+str(args.depth)+'x'+str(args.widen_factor)
     else:
         print('Error : Network should be either [LeNet / VGGNet / ResNet / Wide_ResNet')
         sys.exit(0)
 
-    return net, file_name
+    return file_name
 
 # Test only option
-if (args.testOnly):
+def test_only(testloader):
     print('\n[Test Phase] : Model setup')
     assert os.path.isdir('checkpoint'), 'Error: No checkpoint directory found!'
-    _, file_name = getNetwork(args)
+    file_name = get_model_file_name()
     checkpoint = torch.load('./checkpoint/'+args.dataset+os.sep+file_name+'.t7')
-    net = checkpoint['net']
+    model = checkpoint['net']
 
     if use_cuda:
-        net.cuda()
-        net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
+        model.to(device)
+        if use_cuda_parallel:
+            model = torch.nn.DataParallel(model, device_ids=args.gpu_ids)
         cudnn.benchmark = True
 
-    net.eval()
-    net.training = False
-    test_loss = 0
+    model.eval()
+    model.training = False
     correct = 0
     total = 0
 
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
-            if use_cuda:
-                inputs, targets = inputs.cuda(), targets.cuda()
-            inputs, targets = Variable(inputs), Variable(targets)
+            inputs, targets = inputs.to(device), targets.to(device)
             outputs = net(inputs)
 
             _, predicted = torch.max(outputs.data, 1)
@@ -116,49 +124,49 @@ if (args.testOnly):
             correct += predicted.eq(targets.data).cpu().sum()
 
         acc = 100.*correct/total
-        print("| Test Result\tAcc@1: %.2f%%" %(acc))
-
-    sys.exit(0)
+        print("| Test Result\tAcc@1: %.2f%%" % acc)
 
 # Model
-print('\n[Phase 2] : Model setup')
-if args.resume:
-    # Load checkpoint
-    print('| Resuming from checkpoint...')
-    assert os.path.isdir('checkpoint'), 'Error: No checkpoint directory found!'
-    _, file_name = getNetwork(args)
-    checkpoint = torch.load('./checkpoint/'+args.dataset+os.sep+file_name+'.t7')
-    net = checkpoint['net']
-    best_acc = checkpoint['acc']
-    start_epoch = checkpoint['epoch']
-else:
-    print('| Building net type [' + args.net_type + ']...')
-    net, file_name = getNetwork(args)
-    net.apply(conv_init)
+def setup_model(num_classes):
+    file_name = get_model_file_name()
+    if args.resume:
+        # Load checkpoint
+        print('| Resuming from checkpoint...')
+        assert os.path.isdir('checkpoint'), 'Error: No checkpoint directory found!'
+        checkpoint = torch.load('./checkpoint/'+args.dataset+os.sep+file_name+'.t7')
+        model = checkpoint['net']
+        global best_acc, start_epoch
+        best_acc = checkpoint['acc']
+        start_epoch = checkpoint['epoch']
+    else:
+        print('| Building net type [' + args.net_type + ']...')
+        model = get_model(num_classes)
+        model.apply(conv_init)
 
-if use_cuda:
-    net.cuda()
-    net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
-    cudnn.benchmark = True
-
-criterion = nn.CrossEntropyLoss()
+    if use_cuda:
+        model.to(device)
+        if use_cuda_parallel:
+            model = torch.nn.DataParallel(model, device_ids=args.gpu_ids)
+        cudnn.benchmark = True
+    return model, file_name
 
 # Training
-def train(epoch):
-    net.train()
-    net.training = True
+def train(epoch, model, trainloader, criterion):
+    model.train()
+    model.training = True
     train_loss = 0
     correct = 0
     total = 0
-    optimizer = optim.SGD(net.parameters(), lr=cf.learning_rate(args.lr, epoch), momentum=0.9, weight_decay=5e-4)
+    lr = cf.learning_rate(args.lr, epoch)
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
 
-    print('\n=> Training Epoch #%d, LR=%.4f' %(epoch, cf.learning_rate(args.lr, epoch)))
-    for batch_idx, (inputs, targets) in enumerate(trainloader):
-        if use_cuda:
-            inputs, targets = inputs.cuda(), targets.cuda() # GPU settings
+    print('\n=> Training Epoch #%d, LR=%.4f' % (epoch, lr))
+    b_cnt = len(trainloader)
+    for b_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device) # GPU settings
         optimizer.zero_grad()
         inputs, targets = Variable(inputs), Variable(targets)
-        outputs = net(inputs)               # Forward Propagation
+        outputs = model(inputs)               # Forward Propagation
         loss = criterion(outputs, targets)  # Loss
         loss.backward()  # Backward Propagation
         optimizer.step() # Optimizer update
@@ -170,23 +178,21 @@ def train(epoch):
 
         sys.stdout.write('\r')
         sys.stdout.write('| Epoch [%3d/%3d] Iter[%3d/%3d]\t\tLoss: %.4f Acc@1: %.3f%%'
-                %(epoch, num_epochs, batch_idx+1,
-                    (len(trainset)//batch_size)+1, loss.item(), 100.*correct/total))
+                         % (epoch, num_epochs, b_idx+1, b_cnt, loss.item(), 100.*correct/total))
         sys.stdout.flush()
 
-def test(epoch):
+def test(epoch, model, testloader, criterion, file_name):
     global best_acc
-    net.eval()
-    net.training = False
+    model.eval()
+    model.training = False
     test_loss = 0
     correct = 0
     total = 0
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
-            if use_cuda:
-                inputs, targets = inputs.cuda(), targets.cuda()
+            inputs, targets = inputs.to(device), targets.to(device)
             inputs, targets = Variable(inputs), Variable(targets)
-            outputs = net(inputs)
+            outputs = model(inputs)
             loss = criterion(outputs, targets)
 
             test_loss += loss.item()
@@ -196,14 +202,14 @@ def test(epoch):
 
         # Save checkpoint when best model
         acc = 100.*correct/total
-        print("\n| Validation Epoch #%d\t\t\tLoss: %.4f Acc@1: %.2f%%" %(epoch, loss.item(), acc))
+        print("\n| Validation Epoch #%d\t\t\tLoss: %.4f Acc@1: %.2f%%" % (epoch, loss.item(), acc))
 
         if acc > best_acc:
-            print('| Saving Best model...\t\t\tTop1 = %.2f%%' %(acc))
+            print('| Saving Best model...\t\t\tTop1 = %.2f%%' % acc)
             state = {
-                    'net':net.module if use_cuda else net,
-                    'acc':acc,
-                    'epoch':epoch,
+                    'net': net.module if use_cuda_parallel else net,
+                    'acc': acc,
+                    'epoch': epoch,
             }
             if not os.path.isdir('checkpoint'):
                 os.mkdir('checkpoint')
@@ -213,21 +219,36 @@ def test(epoch):
             torch.save(state, save_point+file_name+'.t7')
             best_acc = acc
 
-print('\n[Phase 3] : Training model')
-print('| Training Epochs = ' + str(num_epochs))
-print('| Initial Learning Rate = ' + str(args.lr))
-print('| Optimizer = ' + str(optim_type))
+def main():
+    print('\n[Phase 1] : Data Preparation')
+    trainloader, testloader, num_classes = get_data_loader()
 
-elapsed_time = 0
-for epoch in range(start_epoch, start_epoch+num_epochs):
-    start_time = time.time()
+    if args.testOnly:
+        test_only(testloader)
+        return
 
-    train(epoch)
-    test(epoch)
+    print('\n[Phase 2] : Model setup')
+    model, file_name = setup_model(num_classes)
 
-    epoch_time = time.time() - start_time
-    elapsed_time += epoch_time
-    print('| Elapsed time : %d:%02d:%02d'  %(cf.get_hms(elapsed_time)))
+    print('\n[Phase 3] : Training model')
+    print('| Training Epochs = ' + str(num_epochs))
+    print('| Initial Learning Rate = ' + str(args.lr))
 
-print('\n[Phase 4] : Testing model')
-print('* Test results : Acc@1 = %.2f%%' %(best_acc))
+    criterion = nn.CrossEntropyLoss()
+    elapsed_time = 0
+    for epoch in range(start_epoch, start_epoch+num_epochs):
+        start_time = time.time()
+
+        train(epoch, model, trainloader, criterion)
+        test(epoch, model, testloader, criterion, file_name)
+
+        epoch_time = time.time() - start_time
+        elapsed_time += epoch_time
+        print('| Elapsed time : %d:%02d:%02d'  % (cf.get_hms(elapsed_time)))
+
+    print('\n[Phase 4] : Testing model')
+    print('* Test results : Acc@1 = %.2f%%' % best_acc)
+# main()
+
+if __name__ == '__main__':
+    main()
